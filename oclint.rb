475a59c180bbedf0c60b5f5544ae3feba8eec826
valery_demin@epam.com <valery_demin@epam.com>
2013-09-20 16:00:16 +0400

    script for install oclint has been updated

--- oclint.rb
+++ oclint.rb
@@ -2,9 +2,9 @@ require 'formula'
  
 class Oclint < Formula
 homepage 'http://oclint.org'
-url 'http://archives.oclint.org/releases/0.7/oclint-0.7-x86_64-apple-darwin-10.tar.gz'
-version '0.6'
-sha1 '9fb4bdfb6dbce14986d863d8b672870c0ae08ec8'
+url 'http://archives.oclint.org/releases/0.6/oclint-0.6-x86_64-apple-darwin12.tar.gz'
+version '0.7'
+sha1 '867751f9e1b73515c22a014b22592b31c92f81bb'
  
 depends_on 'llvm'
  
