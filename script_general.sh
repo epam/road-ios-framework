10dba00a803dd5cbbb462773cced45554042e730
valery-demin <valery_demin@epam.com>
2013-10-02 23:42:34 +0400

    Scripts for Travis have been updated

--- script_general.sh
+++ script_general.sh
@@ -8,4 +8,15 @@ if [[ $SPARK_SCHEME == SparkAttributesCodeGenerator ]]; then export PATCH_FOR_PR
 # =================     Run build, test and oclint check     ===========
 xctool $PATCH_FOR_PROJECT_OR_WORKSPACE -scheme $SPARK_SCHEME -reporter pretty -reporter json-compilation-database:compile_commands.json build
 if [[ $SPARK_SCHEME != SparkAttributesCodeGenerator ]]; then xctool $WORKSPACE -scheme $SPARK_SCHEME test -sdk iphonesimulator; fi	
+
+# =================     Download oclint, unzip and setup    ===========
+wget http://archives.oclint.org/releases/0.7/oclint-0.7-x86_64-apple-darwin-10.tar.gz > /dev/null
+tar xzf oclint-0.7-x86_64-apple-darwin-10.tar.gz > /dev/null
+# =================     Remove necessary rules from "lib/oclint/rules/" folder of oclint     ===========
+#- rm $('pwd')/oclint-0.7-x86_64-apple-darwin-10/lib/oclint/rules/libUnusedMethodParameterRule.dylib
+# ======================================================================================================
+OCLINT_HOME=$('pwd')/oclint-0.7-x86_64-apple-darwin-10
+PATH=$OCLINT_HOME/bin:$PATH
+
+# =================     Run oclint    ===========
 oclint-json-compilation-database -- -rc=LONG_LINE=300 -rc=LONG_VARIABLE_NAME=50 -max-priority-2 30 -max-priority-3 80
