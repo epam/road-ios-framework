91da08ac912cb26d3735a4c82fb7728969e7484c
valery-demin <valery_demin@epam.com>
2013-10-07 17:40:47 +0400

    Script for Coverage Analysis has been added to .travis.yml

--- script_general.sh
+++ script_general.sh
@@ -5,16 +5,22 @@ export WORKSPACE="-workspace Framework/SparkFramework.xcworkspace"
 export PROJECT="-project tools/SparkAttributesCodeGenerator/SparkAttributesCodeGenerator.xcodeproj"
 if [[ $SPARK_SCHEME == SparkAttributesCodeGenerator ]]; then export PATCH_FOR_PROJECT_OR_WORKSPACE=$PROJECT; else export PATCH_FOR_PROJECT_OR_WORKSPACE=$WORKSPACE; fi
 
+# =================     Install cpp-coveralls    ===========
+sudo easy_install cpp-coveralls > /dev/null
+
 # =================     Run build, test and oclint check     ===========
 xctool $PATCH_FOR_PROJECT_OR_WORKSPACE -scheme $SPARK_SCHEME -reporter pretty -reporter json-compilation-database:compile_commands.json build
 if [[ $SPARK_SCHEME != SparkAttributesCodeGenerator ]]; then xctool $WORKSPACE -scheme $SPARK_SCHEME test -sdk iphonesimulator; fi	
 
-# =================     Download oclint, unzip and setup    ===========
+# =================     Download oclint, unzip    ===========
 wget http://archives.oclint.org/releases/0.7/oclint-0.7-x86_64-apple-darwin-10.tar.gz > /dev/null
 tar xzf oclint-0.7-x86_64-apple-darwin-10.tar.gz > /dev/null
+
 # =================     Remove necessary rules from "lib/oclint/rules/" folder of oclint     ===========
 #- rm $('pwd')/oclint-0.7-x86_64-apple-darwin-10/lib/oclint/rules/libUnusedMethodParameterRule.dylib
 # ======================================================================================================
+
+# =================     Setup oclint    ===========
 OCLINT_HOME=$('pwd')/oclint-0.7-x86_64-apple-darwin-10
 PATH=$OCLINT_HOME/bin:$PATH
 
