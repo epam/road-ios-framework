c455dcc39bd0e130a00d048b63c5d528aeb30ac8
nikita-leonov <nikita.leonov@gmail.com>
2013-10-10 08:40:27 -0700

    Turning off unused parameter oclint check

--- script_general.sh
+++ script_general.sh
@@ -17,7 +17,7 @@ wget http://archives.oclint.org/releases/0.7/oclint-0.7-x86_64-apple-darwin-10.t
 tar xzf oclint-0.7-x86_64-apple-darwin-10.tar.gz > /dev/null
 
 # =================     Remove necessary rules from "lib/oclint/rules/" folder of oclint     ===========
-#- rm $('pwd')/oclint-0.7-x86_64-apple-darwin-10/lib/oclint/rules/libUnusedMethodParameterRule.dylib
+rm $('pwd')/oclint-0.7-x86_64-apple-darwin-10/lib/oclint/rules/libUnusedMethodParameterRule.dylib
 # ======================================================================================================
 
 # =================     Setup oclint    ===========
