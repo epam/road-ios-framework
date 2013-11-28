3bee84412ac419eddbc36ab9407e1e7ab91b0bc7
Yuru Taustahuzau <yuru_taustahuzau@epam.com>
2013-11-28 13:46:49 +0300

    #207. Fixing wrong alignments for test serialization file

--- Framework/ROADSerialization/ROADSerializationTest/Classes/RFAnnotatedCoderTest.m
+++ Framework/ROADSerialization/ROADSerializationTest/Classes/RFAnnotatedCoderTest.m
@@ -61,7 +61,7 @@
 - (void)testSerialization {
     NSString *result = [RFAttributedCoder encodeRootObject:object];
     NSError *error;
-    NSString *test = [NSString stringWithContentsOfFile:[[NSBundle bundleForClass:self.class]  pathForResource:@"SerializationTest" ofType:@"json"] encoding:NSUTF8StringEncoding error:&error];
+    NSString *test = [NSString stringWithContentsOfFile:[[NSBundle bundleForClass:self.class] pathForResource:@"SerializationTest" ofType:@"json"] encoding:NSUTF8StringEncoding error:&error];
     STAssertNil(error, @"Assertion: SerializationTest.json file was not loaded to check result");
     STAssertTrue([test isEqualToString:result], @"Assertion: serialization is successful.");
 }
