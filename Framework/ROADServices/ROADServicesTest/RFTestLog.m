0ef46a4a5f24120ff4526fc7ca3f5f9ae144e3e5
Yuru Taustahuzau <yuru_taustahuzau@epam.com>
2013-12-08 20:13:35 +0300

    Enabling test coverage for all targets

---
+++ Framework/ROADServices/ROADServicesTest/RFTestLog.m
@@ -0,0 +1,54 @@
+//
+//  RFTestLog.m
+//  ROADServices
+//
+//  Created by Yuru Taustahuzau on 12/8/13.
+//  Copyright (c) 2013 Epam Systems. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+// GCOV Flush function
+extern void __gcov_flush(void);
+
+static id mainSuite = nil;
+
+@interface RFTestLog : SenTestLog
+
+@end
+
+@implementation RFTestLog
+
++ (void)initialize
+{
+	[[NSUserDefaults standardUserDefaults] setValue:@"RFTestLog" forKey:SenTestObserverClassKey];
+    
+	[super initialize];
+}
+
++ (void)testSuiteDidStart:(NSNotification *)notification
+{
+	[super testSuiteDidStart:notification];
+    
+	SenTestSuiteRun *suite = notification.object;
+    
+	if (mainSuite == nil)
+	{
+		mainSuite = suite;
+	}
+}
+
++ (void)testSuiteDidStop:(NSNotification *)notification
+{
+	[super testSuiteDidStop:notification];
+    
+	SenTestSuiteRun* suite = notification.object;
+    
+	if (mainSuite == suite)
+	{
+		// workaround for missing flush with iOS 7 Simulator
+		__gcov_flush();
+	}
+}
+
+@end
