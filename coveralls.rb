83e8066c2ad75102bcb4d743217e0936565ff6b4
Yuru Taustahuzau <yuru_taustahuzau@epam.com>
2013-12-10 13:24:29 +0300

    Enabling verbose logging for coveralls.rb

--- coveralls.rb
+++ coveralls.rb
@@ -47,6 +47,7 @@ end
 # the folders
 workingDir = Dir.getwd
 derivedDataDir = "#{Etc.getpwuid.dir}/Library/Developer/Xcode/DerivedData/"
+puts "DerivedData folder: #{derivedDataDir}"
 outputDir = workingDir + "/gcov"
 
 # create gcov output folder
@@ -57,7 +58,7 @@ GCOV_SOURCE_PATTERN = Regexp.new(/Source:(.*)/)
 
 # enumerate all gcda files underneath derivedData
 Find.find(derivedDataDir) do |gcda_file|
-
+  puts "File founded: #{gcda_file}"
   if gcda_file.match(/\.gcda\Z/)
     
       #get just the folder name
