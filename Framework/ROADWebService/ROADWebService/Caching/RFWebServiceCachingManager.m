e4d7d8d5100981d462afe73d8752d8142aa5e805
Yuru Taustahuzau <yuru_taustahuzau@epam.com>
2013-12-13 17:29:45 +0300

    #166. Adding basic implementation for Cache Manager

---
+++ Framework/ROADWebService/ROADWebService/Caching/RFWebServiceCachingManager.m
@@ -0,0 +1,97 @@
+//
+//  RFWebServiceCachingManager.m
+//  ROADWebService
+//
+//  Copyright (c) 2013 Epam Systems. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//  Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//  Redistributions in binary form must reproduce the above copyright notice, this
+// list of conditions and the following disclaimer in the documentation and/or
+// other materials provided with the distribution.
+//  Neither the name of the EPAM Systems, Inc.  nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// See the NOTICE file and the LICENSE file distributed with this work
+// for additional information regarding copyright ownership and licensing
+
+#import "RFWebServiceCachingManager.h"
+#import <CoreData/CoreData.h>
+#import <ROAD/ROADCore.h>
+#import <ROAD/ROADLogger.h>
+
+#import "RFWebResponse.h"
+#import "RFWebServiceCacheContext.h"
+
+
+static NSString * const kRFWebResponseEntityName = @"WebResponse";
+
+const char * RFWebServiceCacheQueueName = "RFWebServiceCacheQueue";
+
+@implementation RFWebServiceCachingManager {
+    dispatch_queue_t _cacheQueue;
+    
+    RFWebServiceCacheContext * _cacheContext;
+}
+
+
+#pragma mark - Initialization
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        _cacheQueue = dispatch_queue_create(RFWebServiceCacheQueueName, DISPATCH_QUEUE_SERIAL);
+        _cacheContext = [[RFWebServiceCacheContext alloc] init];
+    }
+    return self;
+}
+
+- (void)setCacheWithURL:(NSString *const)URLString requestBodyData:(NSData *const)requestBodyData response:(NSHTTPURLResponse *const)response responseBodyData:(NSData *const)responseBodyData expirationDate:(NSDate *)expirationDate {
+    if ([expirationDate compare:[NSDate date]] == NSOrderedAscending) {
+        dispatch_sync(_cacheQueue, ^{
+            NSManagedObjectContext *managedObjectContext = _cacheContext.context;
+            
+            RFWebResponse *newWebResponse = [NSEntityDescription insertNewObjectForEntityForName:kRFWebResponseEntityName inManagedObjectContext:_cacheContext.context];
+            newWebResponse.urlHash = [[NSDecimalNumber alloc] initWithUnsignedInteger:[URLString hash]];
+            newWebResponse.requestURL = URLString;
+            newWebResponse.requestBodyData = requestBodyData;
+            newWebResponse.response = [NSKeyedArchiver archivedDataWithRootObject:response];
+            newWebResponse.responseBodyData = responseBodyData;
+            newWebResponse.expirationDate = expirationDate;
+            
+            NSError *error;
+            [managedObjectContext save:&error];
+            RFLogError(@"RFWebServiceCachingManager error: saving cached response failed with error: %@", [error localizedDescription]);
+        });
+
+    }
+}
+
+- (id)cacheWithURL:(NSString *const)URLString requestBodyData:(NSData *const)requestBodyData {
+    __block NSArray *cachedResponse;
+    dispatch_sync(_cacheQueue, ^{
+        NSManagedObjectContext *managedObjectContext = _cacheContext.context;
+        NSFetchRequest *fetchCachedResponse = [[NSFetchRequest alloc] initWithEntityName:kRFWebResponseEntityName];
+        NSError *error;
+        cachedResponse = [managedObjectContext executeFetchRequest:fetchCachedResponse error:&error];
+    });
+        
+    return cachedResponse;
+}
+
+@end
